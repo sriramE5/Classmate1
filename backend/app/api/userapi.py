b3904e9a57b577394dd8bf87badf458f9368dd7f
ganesh714 <evvganesh1@gmail.com>
2025-05-18 22:42:24 +0530

    by ganesh

--- backend/app/api/userapi.py
+++ backend/app/api/userapi.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi import APIRouter,FastAPI, HTTPException, Depends, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel, validator
