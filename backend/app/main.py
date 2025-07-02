8ff5990b43d1043b627272021b929bcf04d9fb77
sriramE5 <153256696+sriramE5@users.noreply.github.com>
2025-07-03 01:25:44 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,21 +1,25 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api import userapi, chatbotapi
+from app.api import userapi, chatbotapi, notesapi, tasksapi, chathistoryapi
 
 app = FastAPI()
 
 # Enable CORS (adjust origins in production)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://ganesh714.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Include routers
-app.include_router(userapi.router )
+# Include all routers
+app.include_router(userapi.router)
 app.include_router(chatbotapi.router)
+app.include_router(notesapi.router)
+app.include_router(tasksapi.router)
+app.include_router(chathistoryapi.router)
+
 
 @app.get("/")
 async def root():
